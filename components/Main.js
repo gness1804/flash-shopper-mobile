995f0da919a2708a528922aca85c1ee89d8f1f41
gness1804 <graham.nessler@gmail.com>
2017-04-10 19:35:23 -0500

    Fix up some linting issues

--- components/Main.js
+++ components/Main.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
   Text,
   TextInput,
   View,
@@ -11,7 +9,7 @@ import {
 import styles from '../styles/Main-styles';
 
 class Main extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       name: '',
@@ -22,7 +20,7 @@ class Main extends Component {
     }
   }
 
-  createItem(){
+  createItem = () => {
     const newItem = {
       name: this.state.name,
       aisle: this.state.aisle,
@@ -30,119 +28,117 @@ class Main extends Component {
       quantity: this.state.quantity,
       id: Date.now(),
     };
-  this.props.addNewItem(newItem);
-  this.setState(this.state)
-  this.setState({ name: '' })
-  this.clearText('itemInput', 'aisleInput', 'noteInput', 'quantityInput');
+    this.props.addNewItem(newItem);
+    this.setState(this.state)
+    this.setState({ name: '' })
+    this.clearText('itemInput', 'aisleInput', 'noteInput', 'quantityInput');
   }
 
-  clearText(itemInput, aisleInput, noteInput, quantityInput){
-    this.refs[itemInput].setNativeProps({text: ''});
-    this.refs[aisleInput].setNativeProps({text: ''});
-    this.refs[noteInput].setNativeProps({text: ''});
-    this.refs[quantityInput].setNativeProps({text: ''});
+  clearText = (itemInput, aisleInput, noteInput, quantityInput) => {
+    this.refs[itemInput].setNativeProps({ text: '' });
+    this.refs[aisleInput].setNativeProps({ text: '' });
+    this.refs[noteInput].setNativeProps({ text: '' });
+    this.refs[quantityInput].setNativeProps({ text: '' });
   }
 
-  deleteAllItems(){
+  deleteAllItems = () => {
     this.props.deleteAllItems();
   }
 
-  deleteItem(id){
+  deleteItem = (id) => {
     this.props.deleteItem(id);
   }
 
-  sortByAisle(){
+  sortByAisle = () => {
     this.props.sortByAisle();
   }
 
-  sortAlpha(){
+  sortAlpha = () => {
     this.props.sortAlpha();
   }
 
   render() {
+  // const { name, aisle, note, quantity, id } = this.state;
+    const { items } = this.props;
 
-  const { name, aisle, note, quantity, id } = this.state;
-  const { items } = this.props;
+    let itemsDisplay;
 
-  let itemsDisplay;
-
-  if (items.length > 0) {
-    itemsDisplay = items.map((item) => {
-      return <View style={styles.eachItemContainer} key={item.id}>
-      <Text style={styles.text}>Item: {item.name}</Text>
-      <Text style={styles.text}>Aisle: {item.aisle}</Text>
-      <Text style={styles.text}>Note: {item.note}</Text>
-      <Text style={styles.text}>Quantity: {item.quantity}</Text>
-      <Button
-        title="Delete Item"
-        style={styles.redButtons}
-        onPress={() => { this.deleteItem(item.id) }}
-      />
-      </View>
-    })
-  }
+    if (items.length > 0) {
+      itemsDisplay = items.map((item) => {
+        return (<View style={styles.eachItemContainer} key={item.id}>
+          <Text style={styles.text}>Item: {item.name}</Text>
+          <Text style={styles.text}>Aisle: {item.aisle}</Text>
+          <Text style={styles.text}>Note: {item.note}</Text>
+          <Text style={styles.text}>Quantity: {item.quantity}</Text>
+          <Button
+            title="Delete Item"
+            style={styles.redButtons}
+            onPress={() => { this.deleteItem(item.id) }}
+          />
+        </View>)
+      })
+    }
 
     return (
       <ScrollView>
-      <TextInput
-        id="item-input"
-        value={this.state.name}
-        style={styles.inputField}
-        ref={'itemInput'}
-        placeholder="Item Name"
-        onChangeText={(name) => this.setState({name})}
-      />
-      <TextInput
-        id="aisle-input"
-        value={this.state.aisle}
-        style={styles.inputField}
-        ref={'aisleInput'}
-        placeholder="Aisle Name"
-        onChangeText={(aisle) => this.setState({aisle})}
-      />
-      <TextInput
-        id="note-input"
-        value={this.state.note}
-        style={styles.inputField}
-        ref={'noteInput'}
-        placeholder="Note"
-        onChangeText={(note) => this.setState({note})}
-      />
-      <TextInput
-        id="quantity-input"
-        value={this.state.quantity}
-        style={styles.inputField}
-        ref={'quantityInput'}
-        placeholder="Quantity"
-        onChangeText={(quantity) => this.setState({quantity})}
-      />
-      <Button
-      title="Submit"
-      style={styles.greenButtons}
-      disabled={!this.state.name}
-      onPress={(name, aisle, note, quantity) => { this.createItem(name, aisle, note, quantity) }}
-      />
-      <Button
-        title="Sort by Aisle"
+        <TextInput
+          id="item-input"
+          value={this.state.name}
+          style={styles.inputField}
+          ref={'itemInput'}
+          placeholder="Item Name"
+          onChangeText={(name) => this.setState({name})}
+        />
+        <TextInput
+          id="aisle-input"
+          value={this.state.aisle}
+          style={styles.inputField}
+          ref={'aisleInput'}
+          placeholder="Aisle Name"
+          onChangeText={(aisle) => this.setState({aisle})}
+        />
+        <TextInput
+          id="note-input"
+          value={this.state.note}
+          style={styles.inputField}
+          ref={'noteInput'}
+          placeholder="Note"
+          onChangeText={(note) => this.setState({note})}
+        />
+        <TextInput
+          id="quantity-input"
+          value={this.state.quantity}
+          style={styles.inputField}
+          ref={'quantityInput'}
+          placeholder="Quantity"
+          onChangeText={(quantity) => this.setState({quantity})}
+        />
+        <Button
+        title="Submit"
         style={styles.greenButtons}
-        disabled={this.props.items.length === 0}
-        onPress={() => { this.sortByAisle() }}
-      />
-      <Button
-        title="Sort Alpha"
-        style={styles.greenButtons}
-        disabled={this.props.items.length === 0}
-        onPress={() => { this.sortAlpha() }}
-      />
-      <Button
-        title="Delete ALL Items"
-        style={styles.redButtons}
-        onPress={() => { this.deleteAllItems() }}
-        disabled={this.props.items.length === 0}
-      />
-      {itemsDisplay}
-    </ScrollView>
-
+        disabled={!this.state.name}
+        onPress={(name, aisle, note, quantity) => { this.createItem(name, aisle, note, quantity) }}
+        />
+        <Button
+          title="Sort by Aisle"
+          style={styles.greenButtons}
+          disabled={this.props.items.length === 0}
+          onPress={() => { this.sortByAisle() }}
+        />
+        <Button
+          title="Sort Alpha"
+          style={styles.greenButtons}
+          disabled={this.props.items.length === 0}
+          onPress={() => { this.sortAlpha() }}
+        />
+        <Button
+          title="Delete ALL Items"
+          style={styles.redButtons}
+          onPress={() => { this.deleteAllItems() }}
+          disabled={this.props.items.length === 0}
+        />
+        {itemsDisplay}
+      </ScrollView>
     );
   }
 
