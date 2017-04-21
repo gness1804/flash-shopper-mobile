f593d48424424e175e4a61629768a63e5da4e2a1
gness1804 <graham.nessler@gmail.com>
2017-04-21 14:42:49 -0500

    Reformat text in no item view

--- components/NoItemsDisplay.js
+++ components/NoItemsDisplay.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { ScrollView, Text, Image } from 'react-native';
+import styles from '../styles/NoItemsDisplay-styles';
 
 const NoItemsDisplay = () => {
   return (
     <ScrollView>
       <Image
         source={require('../images/cart-plus.png')}
-
+        style={styles.cart}
       />
-      <Text>There are no items on your list. To add an item, click the "plus" icon near the top of the screen.</Text>
+      <Text
+        style={styles.text}
+      >
+        There are no items on your list. To add an item, click the "plus" icon near the top of the screen.
+      </Text>
     </ScrollView>
   );
 }
---
+++ styles/NoItemsDisplay-styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  cart: {
+    alignSelf: 'center',
+    height: 200,
+    marginTop: 100,
+    width: 250,
+  },
+  text: {
+    marginTop: 60,
+    textAlign: 'center',
+  },
+})
+
+export default styles
